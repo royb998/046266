92dd34912ee81f3ba666dd6052efba45387e7956
Roy Bar-On <royb997@gmail.com>
2025-02-01 17:41:55 +0200

    Added more tests, unified testing script

--- run_tests.sh
+++ run_tests.sh
@@ -1,40 +1,59 @@
 #!/bin/bash
+
 make clean
 make &> build.log
 
-rm -rf asm bin outputs
-mkdir -p asm bin outputs
-for i in $(seq 1 10)
+rm -rf asm bin outputs diffs
+mkdir -p asm bin outputs diffs
+for testdir in tests/*
 do 
-    echo test $i
+    export testname=`basename $testdir`
+    echo Testing $testname
 
     #echo building...
-    ./rx-cc tests/test$i/test.cmm
+    ./rx-cc tests/$testname/test.cmm
     if [ $? -ne 0  ]; then
-        echo failed to build test$i
+        echo failed to build $testname
+        echo
+        continue
+    fi
+    mv test.rsk asm/$testname.rsk
+
+    if [[ -d "tests/$testname/includes" && -n "$( ls -A tests/$testname/includes )" ]]; then
+        mkdir -p asm/inc_$testname
+        for filename in tests/$testname/includes/*.cmm; do
+            export name=`basename $filename .cmm`
+            ./rx-cc $filename
+            mv $name.rsk asm/inc_$testname
+        done
+    fi
+
+    #echo linking...
+    if [[ -d "tests/$testname/includes" && -n "$( ls -A tests/$testname/includes )" ]]; then
+        ./rx-linker asm/$testname.rsk asm/inc_$testname/*
+    else
+        ./rx-linker asm/$testname.rsk      
+    fi
+
+    #echo running...
+    mv asm/$testname.e bin
+    touch tests/$testname/input.in tests/$testname/output.out
+    ./rx-vm-nprints bin/$testname.e < tests/$testname/input.in 1> outputs/$testname.out
+    if [ $? -ne 0 ]; then
+        echo Runtime error on $testname
         echo
         continue
     fi
 
-    #echo linking...
-    mv test.rsk asm/test$i.rsk
-    ./rx-linker asm/test$i.rsk
-
-    #echo running...
-    mv asm/test$i.e bin
-    ./rx-vm bin/test$i.e < tests/test$i/input.in 1> outputs/test$i.out
-    if [ $? -ne 0 ]; then
-        echo Runtime error on test $i
-        continue
-    fi
-
     #echo comparing...
-    diff tests/test$i/output.out outputs/test$i.out 1>outputs/test$i.diff
+    diff tests/$testname/output.out outputs/$testname.out 1>diffs/$testname.diff
     if [ $? -ne 0 ]; then
-        echo test$i failed! Different outputs.
+        echo $testname failed! Different outputs.
     else
-        echo test$1 succeeded!
+        echo $testname succeeded!
     fi
 
     echo
 done
+
+rm -rf test.rsk *.e
